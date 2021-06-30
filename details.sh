1d4de63f4718bdca8f322f9382285c760c242f75
divya <divyaprabhu422@gmail.com>
2021-06-30 09:34:09 +0000

    modified details.sh script by addind random password lines

--- details.sh
+++ details.sh
@@ -6,5 +6,20 @@ read name
 echo "enter your location"
 read location
 
+#generating random password
+
+echo "Do u want to generate random password:yes or No"
+read option
+if [ $option == "yes" ]
+then
+echo "generating password....."
+passwd1=$(date +%s)$RANDOM
+
+echo $name password is $passwd1 >> password
+echo "your password is generated and stored in password file"
+else
+echo "You have selected not to generate password"
+fi
+
 echo $name >>output
 echo $location >>output
